1d7dbacca481a27e87c28439ea509949081efc89
Andrew Nelson <andyfaff@gmail.com>
2020-09-04 12:55:54 +1000

    update

---
+++ minimize/Minimize.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Minimize(object):
+    def __init__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def solve(self):
+        pass
+
+    @property
+    def x(self):
+        "The Solution vector"
+        pass
+
+    def converged(self):
+        # the truth of whether a solve has finished
+        pass
+
+    @property
+    def result(self):
+        # the OptimizeResult associated with the minimizer
+        pass
